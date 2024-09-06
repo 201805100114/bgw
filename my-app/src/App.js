d7a47c53760c57829cd7355ee8fb22f34c89768b
201805100114 <120076968+201805100114@users.noreply.github.com>
2024-09-06 09:15:13 +0000

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import MyPage from './MyPage'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <MyPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/MyPage.js
@@ -0,0 +1,16 @@
+// my-app/src/MyPage.js
+import React from 'react';
+import './MyPage.css';
+
+function MyPage() {
+  return (
+    <div className="MyPage">
+      <header className="MyPage-header">
+        <h1>Welcome to My Page</h1>
+        <p>This is a custom page.</p>
+      </header>
+    </div>
+  );
+}
+
+export default MyPage;
